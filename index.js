38aa10973519d16abcd103ec6dbfccacbdf80fdd
Ubuntu <ubuntu@ip-172-31-20-61.eu-central-1.compute.internal>
2015-03-04 11:23:07 +0000

    odified index.js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buf = fs.readFileSync('index.html');
+
+var message = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(message)
 })
 
 app.listen(app.get('port'), function() {
