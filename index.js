2e08958d15917980a6f932242a16d9f4f61fef69
Ubuntu <ubuntu@ip-172-31-20-61.eu-central-1.compute.internal>
2015-03-11 15:45:41 +0000

    commented buggy if

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var fs = require('fs');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+//if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
 
-      fs.readFile(__dirname + '/public/css/style.css', function (err, data) {
-        if (err) console.log(err);
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(data);
-        res.end();
-      });
+//      fs.readFile(__dirname + '/public/css/style.css', function (err, data) {
+  //      if (err) console.log(err);
+    //    res.writeHead(200, {'Content-Type': 'text/css'});
+//        res.write(data);
+  //      res.end();
+    //  });
 
 var message = fs.readFileSync('index.html').toString();
 
