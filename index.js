f7aca717cf0af9a209ad1dcb32dc6458575347ab
peskat <peskat@gmail.com>
2015-04-26 19:03:58 +0000

    Modified copy on index.html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 //if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
 
@@ -17,9 +17,9 @@ app.use(express.static(__dirname + '/public'))
 var message = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send(message)
-})
+  response.send(message);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
